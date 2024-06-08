00edfa96b6ed0e03fe434475b8159572a91303d5
rifqii19 <rifqiabc9@gmail.com>
2024-06-08 14:06:31 +0700

    first commit

--- app.py
+++ app.py
@@ -5,19 +5,19 @@ import os
 import datetime
 
 client = MongoClient('mongodb+srv://rifqiabc9:rifqi123@cluster0.us7dlk3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-db = client.fgd2
+db = client.heavenheart
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/')
 def dashboard():
-    fruit_collection =list(db.fruits.find().sort('_id', DESCENDING))
+    fruit_collection =list(db.produtcs.find().sort('_id', DESCENDING))
     return render_template('dashboard.html', fruit_collection=fruit_collection)
 
 @app.route('/fruits')
 def fruits():
-    fruit_collection =list(db.fruits.find().sort('_id', DESCENDING))
+    fruit_collection =list(db.produtcs.find().sort('_id', DESCENDING))
     return render_template('fruits.html', fruit_collection=fruit_collection)
 
 @app.route('/fruit/add', methods=['GET', 'POST'])
@@ -40,17 +40,17 @@ def add_fruit():
             filename = f"fruit-{datetime.datetime.now().strftime('%H-%M-%S')}.{ext}"
             image.save(f"{save_to}/{filename}")
 
-        db.fruits.insert_one({
+        db.produtcs.insert_one({
             'name': name, 'price': price, 'description': description, 'image': filename
         })
 
-        flash('Berhasil menambahkan data buah!')
+        flash('Berhasil menambahkan data produtcs')
         return redirect(url_for('fruits'))
 
 @app.route('/fruit/edit/<id>', methods=['GET', 'POST'])
 def edit_fruit(id):
     if request.method == 'GET':
-        fruit = db.fruits.find_one({'_id': ObjectId(id)})
+        fruit = db.produtcs.find_one({'_id': ObjectId(id)})
         return render_template('edit-fruit.html', fruit=fruit)
 
     else:
@@ -61,7 +61,7 @@ def edit_fruit(id):
         
         if image:
             save_to = 'static/uploads'
-            fruit = db.fruits.find_one({'_id': ObjectId(id)})
+            fruit = db.produtcs.find_one({'_id': ObjectId(id)})
             target = f"static/uploads/{fruit['image']}"
 
             if os.path.exists(target):
@@ -71,24 +71,24 @@ def edit_fruit(id):
             filename = f"fruit-{datetime.datetime.now().strftime('%H-%M-%S')}.{ext}"
             image.save(f"{save_to}/{filename}")
 
-            db.fruits.update_one({'_id': ObjectId(id)}, {'$set': {'image': filename}})
+            db.produtcs.update_one({'_id': ObjectId(id)}, {'$set': {'image': filename}})
         
-        db.fruits.update_one({'_id': ObjectId(id)}, {'$set':{'name': name, 'price': price, 'description': description}})
+        db.produtcs.update_one({'_id': ObjectId(id)}, {'$set':{'name': name, 'price': price, 'description': description}})
             
-        flash('Berhasil mengubah data buah!')
+        flash('Berhasil mengubah data produtcs!')
         return redirect(url_for('fruits'))
     
 @app.route('/fruit/delete/<id>', methods=['POST'])
 def delete_fruit(id):
-    fruit = db.fruits.find_one({'_id': ObjectId(id)})
+    fruit = db.produtcs.find_one({'_id': ObjectId(id)})
     target = f"static/uploads/{fruit['image']}"
 
     if os.path.exists(target):
         os.remove(target)
 
-    db.fruits.delete_one({'_id': ObjectId(id)})
+    db.produtcs.delete_one({'_id': ObjectId(id)})
 
-    flash('Berhasil menghapus data buah!')
+    flash('Berhasil menghapus data produtcs!')
     return redirect(url_for('fruits'))
 
 if __name__ == '__main__':
